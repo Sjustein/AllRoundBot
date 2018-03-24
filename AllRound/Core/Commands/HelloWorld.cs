fadc7fed940b418ff68350a413a7aad1e7884f88
Justin Ruiter <justin.ruiter02@gmail.com>
2018-03-24 17:26:10 +0100

    Add project files.

---
+++ AllRound/Core/Commands/HelloWorld.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Discord;
+using Discord.Commands;
+
+namespace AllRound.Core.Commands
+{
+    public class HelloWorld : ModuleBase<SocketCommandContext>
+    {
+        [Command("hello"), Alias("helloworld", "world"), Summary("Hello world command")]
+        public async Task Sjustein()
+        {
+            await Context.Channel.SendMessageAsync("Hello world");
+        }
+
+        [Command("embed"), Summary("Embed test command")]
+        public async Task Embed([Remainder]string Input = "None")
+        {
+            EmbedBuilder Embed = new EmbedBuilder();
+            Embed.WithAuthor("Test embed", Context.User.GetAvatarUrl());
+            Embed.WithColor(40, 200, 150);
+            Embed.WithFooter("The footer of the embed", Context.Guild.Owner.GetAvatarUrl());
+            Embed.WithDescription("This is a **dummy** description, with a cool link.\n" +
+                              "[This is my favourite website](https://www.sjustein.com/)");
+            Embed.AddInlineField("User input:", Input);
+
+            await Context.Channel.SendMessageAsync("", false, Embed.Build());
+        }
+    }
+}
---
+++ AllRound/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace AllRound
+{
+    class Program
+    {
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+
+        static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+
+            //help help HELP heLp
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            Client.MessageReceived += Client_MessageReceived;
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            Client.Ready += Client_Ready;
+            Client.Log += Client_Log;
+
+            string Token = "";
+            using (var Stream = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.0", @"Data\Token.txt"), FileMode.Open, FileAccess.Read))
+            using (var ReadToken = new StreamReader(Stream))
+            {
+                Token = ReadToken.ReadToEnd();
+            }
+
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Log(LogMessage Message)
+        {
+            Console.WriteLine($"{DateTime.Now} at {Message.Source}] {Message.Message}");
+        }
+
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync("AllRound Bot - Tutorial!", "https://www.sjustein.com/", StreamType.NotStreaming);
+        }
+
+        private async Task Client_MessageReceived(SocketMessage MessageParam)
+        {
+            var Message = MessageParam as SocketUserMessage;
+            var Context = new SocketCommandContext(Client, Message);
+
+            if (Context.Message == null || Context.Message.Content == "") return;
+            if (Context.User.IsBot) return;
+
+            int ArgPos = 0;
+            if (!(Message.HasStringPrefix("a!", ref ArgPos) || Message.HasMentionPrefix(Client.CurrentUser, ref ArgPos))) return;
+
+            var Result = await Commands.ExecuteAsync(Context, ArgPos);
+            if (!Result.IsSuccess)
+                Console.WriteLine($"{DateTime.Now} at Commands] Something went wrong with executing a command. Text: {Context.Message.Content} | Error: {Result.ErrorReason}");
+        }
+    }
+}
